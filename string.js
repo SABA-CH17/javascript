67eb3ffb0b17e40eb380c26278bdc9f59f53339e
Saba Maqbool <162045621+SABA-CH17@users.noreply.github.com>
2024-08-01 13:21:20 +0500

    first commit

---
+++ string.js
@@ -0,0 +1,14 @@
+const name= "javascript";
+const type= "computer language";
+console.log(`Hello, I'm starting a new language named as ${name} ]. It is basically a ${type} commmonly used  in web development.`)
+//Declaration;
+const myname= new String ("Thomass");
+console.log(myname[3]);
+//by using this syntax we can also access many prototypes and objects;
+console.log(myname.length);
+console.log(myname.toUpperCase);
+console.log(myname.charAt[5]);
+const newName= (myname.substring(0,4));
+console.log(newName);
+const anotherName= (myname.slice(-6,5));
+console.log(anotherName);
