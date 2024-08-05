c0152c87133171615773e4ec362a47c00a4a0bb6
Saba Maqbool <162045621+SABA-CH17@users.noreply.github.com>
2024-08-05 21:04:27 +0500

    fist commit

---
+++ maths.js
@@ -0,0 +1,17 @@
+console.log("The value of abstarct is:" , Math.abs(-28));
+console.log("The value of round off is:" , Math.round(7.78));
+console.log("The value of ceil is:" , Math.ceil(4.933));
+console.log("The value of floor is:" , Math.floor(6.39));
+console.log("The value of power is:" , Math.pow(4,7));
+console.log("The value of sqrt is:" , Math.sqrt(73));
+console.log("The maximum value is:" , Math.max(27,74,58,53.94));
+console.log("The minimum value is:" , Math.min(38,04,-35,58,99));
+console.log("The value of random value is:" , Math.random());
+console.log("The value of random fix value is:" , Math.random().toFixed(5));
+// by using formula:
+const max1= 12;
+const min1= 6;
+console.log(Math.floor(Math.random() * (max1- min1+1)+min1));
+
+
+
